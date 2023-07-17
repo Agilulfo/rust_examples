bdf64021c8e4b32c0bc18b8e6c26d949e5b54271
Flavio Di Natale <dinataleflavio@gmail.com>
2023-07-17 21:15:58 +0200

    rename example to example_04

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod example_01;
 mod example_02;
 mod example_03;
+mod example_04;
 
 fn main() {
     // pass a value to a fucion that makes a copy
@@ -9,7 +10,7 @@ fn main() {
     title(1);
     example_01::run();
 
-    // pass a value by reference so that the changes the function makes
+    // pass a value by mutable reference so that the changes the function makes
     // affect the value
     title(2);
     example_02::run();
@@ -17,6 +18,10 @@ fn main() {
     // function
     title(3);
     example_03::run();
+
+    // function
+    title(4);
+    example_04::run();
 }
 
 fn title(number: u8) {
