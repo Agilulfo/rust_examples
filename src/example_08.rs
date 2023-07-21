dbeed8fffb070e2ce6da2833c2d2862cf6482296
Flavio Di Natale <dinataleflavio@gmail.com>
2023-07-21 10:20:26 +0200

    Improve output of modules examples

--- src/example_08.rs
+++ src/example_08.rs
@@ -3,9 +3,9 @@ pub fn run() {
     // defined  in submodules
     // a module can call private objects in the same module or parent module.
 
-    mod_a::a_public_function();
-    mod_a::mod_b::b_public_function();
-    mod_a::mod_b::call_priv_fns();
+    mod_a::a_public_function(String::from("run"));
+    mod_a::mod_b::b_public_function(String::from("run"));
+    mod_a::mod_b::call_priv_fns(String::from("run"));
     let struct_a = mod_a::StA::init(8);
 
     println!("{}", struct_a.public_var);
@@ -29,12 +29,12 @@ mod mod_a {
         }
     }
 
-    fn a_private_function() {
-        println!("private function in mod_a");
+    fn a_private_function(caller: String) {
+        println!("{} called a private function in mod_a", caller);
     }
 
-    pub fn a_public_function() {
-        println!("public function in mod_a");
+    pub fn a_public_function(caller: String) {
+        println!("{} called a public function in mod_a", caller);
     }
 
     pub fn access_private_struct(st_a: StA) {
@@ -43,19 +43,20 @@ mod mod_a {
 
     pub mod mod_b {
 
-        fn b_private_function() {
-            println!("private function in mod_b");
+        fn b_private_function(caller: String) {
+            println!("{} called a private function in mod_b", caller);
         }
 
-        pub fn b_public_function() {
-            println!("public function in mod_b");
+        pub fn b_public_function(caller: String) {
+            println!("{} called a public function in mod_b", caller);
         }
 
         // this function can call private functions
         // in the same module and in the parent modules
-        pub fn call_priv_fns() {
-            super::a_private_function();
-            b_private_function();
+        pub fn call_priv_fns(caller: String) {
+
+            super::a_private_function(caller.clone() + ">call_priv_fns");
+            b_private_function(caller + ">call_priv_fns");
         }
     }
 }
